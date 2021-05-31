ea2c5a3d1811884f91f4a1f903caf5c839d3e756
lvillen <l.villenjaldon@gmail.com>
2021-05-31 20:31:59 +0200

    Sorter class created, working in classes for data

---
+++ src/NumbersCollection.ts
@@ -0,0 +1,17 @@
+export class NumbersCollection {
+  constructor(public data: number[]) {}
+
+  get length(): number {
+    return this.data.length;
+  }
+  
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return this.data[leftIndex] > this.data[rightIndex];
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const tempValue = this.data[leftIndex];
+    this.data[leftIndex] = this.data[rightIndex];
+    this.data[rightIndex] = tempValue;
+  }
+}
