0cfc274a47a09386ffc95e22daa6cadc2b442d2e
lvillen <l.villenjaldon@gmail.com>
2021-05-31 19:42:36 +0200

    A bad approach to the algorithm

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
+        // All of this only works if collection is number[]
+        // If collection is an array of numbers > check with 'instanceof Array'
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHand;
+          }
+        }
+
+        // All of this only works if collection === string > typeof for primitive values
+        // If collection is a string, do this logic instead 
+        // THE CODE
+        if (typeof this.collection === 'string') {
+          
         }
       }
     }
